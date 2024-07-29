b2108805f13dac1469c20cf8eaa6e361b400f4e1
INGENIO <ingeniokhalif@gmail.com>
2024-07-29 15:12:40 +0100

    set up logger with pino

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import createServer from "./base/server";
 import config from "./config";
+import logger from "./utils/logger";
 
 const server = createServer();
 
 // TODO: Change host based on NODE_ENV
-// TODO: Change logger
 server.listen(config.PORT, () =>
-  console.log(`Server running on http://localhost:${config.PORT}. 🚀`)
+  logger.info(`Server running on http://localhost:${config.PORT}.`)
 );
